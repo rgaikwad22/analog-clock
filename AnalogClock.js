319a158da50e557b49c9c501bc98632a4f532fab
rgaikwad22 <rushikeshgaikwad238@gmail.com>
2023-04-08 15:54:39 +0530

    initial commit

---
+++ AnalogClock.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+
+function setDate(){
+    // console.log("Hi")
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; 
+    // console.log(seconds);
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    // console.log(mins);
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    // console.log(hours);
+}
+setInterval(setDate, 1000); 
